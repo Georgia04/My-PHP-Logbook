8db4242b912369ecd572b5b669ae7c8629d78ac6
georgia <georgia@DESKTOP-OEGSNIM>
2021-02-17 10:42:24 -1200

    Update wk1ex7.php

--- Week 1/wk1ex7.php
+++ Week 1/wk1ex7.php
@@ -1,7 +1,13 @@
-$hourlyrate	= 5.75;
+<html>
+<body>
+<?php
+	$hourlyrate	= 5.75;
 	$hoursperweek = 40;
 	$taxrate22 = 0.78;
 	$gross = $hourlyrate * $hoursperweek;
 	$net = $gross * $taxrate22;
-	//echo $gross;
-	echo $net;
+	echo $gross;
+	//echo $net;
+?>
+</body>
+</html>
