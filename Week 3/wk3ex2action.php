d8840d0043987e010d58e1301bdf4e131ffca56f
georgia <georgia@DESKTOP-OEGSNIM>
2021-03-21 06:10:53 -1200

    Week 3 Ex 2 update

---
+++ Week 3/wk3ex2action.php
@@ -0,0 +1,17 @@
+<?php
+if($_POST['txtage']<21){
+    echo "You are under 21 years old<br/>";
+}
+else{
+    echo "You are 21 years old or over<br/>";
+}
+
+if($_POST['txtgender']=="Male")
+{
+    echo "You are male<br/>";
+}
+elseif($_POST['txtgender']=="Female")
+{
+    echo "You are female<br/>";
+}
+?>
