ad662aafa10b953aa54084ff3107db6a83a2a200
georgia <60743148+Georgia04@users.noreply.github.com>
2021-04-14 11:31:02 -1200

    Week 7 ex 2, 3 ,4

---
+++ Week 7/getjpg.php
@@ -0,0 +1,18 @@
+<?php
+
+  header("Content-type: image/jpeg");
+
+  $link = mysqli_connect("localhost", "root", "", "db2_21800490");
+  if (!$link) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+  $sql = "SELECT image FROM monster WHERE id='" . $_GET[id] ."'";
+	
+  $result = mysqli_query($link, $sql);
+  $row = mysqli_fetch_array($result);
+  
+  $jpg = $row["image"];
+
+  echo $jpg;
+?>
